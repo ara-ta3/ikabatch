f8c7adc4c086c6b71e3430603aad72a9132f806d
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2020-09-06 20:55:53 +0900

    Format csv

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as moment from "moment";
 import { fetchDetailedResults, fetchSimpleResults } from "./Gateway";
+import { DetailedResult } from "./Contract";
 
 async function main(argv: string[]): Promise<void> {
   const from = argv[2];
@@ -11,7 +12,24 @@ async function main(argv: string[]): Promise<void> {
   }
   const simpleResults = await fetchSimpleResults(fromDate, toDate);
   const results = await fetchDetailedResults(simpleResults);
-  console.log(results);
+  console.log(formatCSV(results));
+}
+
+function formatCSV(results: DetailedResult[]): string {
+  return results
+    .map((r) => {
+      const players = r.players.sort((x, y) => x.name.localeCompare(y.name));
+      return [
+        r.start.format("YYYY-MM-DD"),
+        "",
+        r.rule,
+        r.stage,
+        r.win ? "勝ち" : "負け",
+      ]
+        .concat(players.map((p) => p.weapon))
+        .join(",");
+    })
+    .join("\n");
 }
 
 main(process.argv).catch((e) => console.error(e));
